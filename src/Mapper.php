d0b9cf7acdb0eb011ceff7410208ee3dd91ac7c3
Evgen Yurchenko <evgen.yurchenko@itechcraft.com>
2024-08-17 09:21:55 +0300

    add map function

---
+++ src/Mapper.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Evgenij\SimpleMapper;
+
+use ReflectionClass;
+use ReflectionException;
+use ReflectionObject;
+
+class Mapper
+{
+    /**
+     * @throws ReflectionException
+     */
+    public function map(object $source, string $destination): object
+    {
+        $destinationReflectionClass = new ReflectionClass($destination);
+        $sourceData = new ReflectionObject($source);
+        $destinationObject = $destinationReflectionClass->newInstanceWithoutConstructor();
+
+        foreach ($destinationReflectionClass->getProperties() as $destinationProperty) {
+            if ($sourceData->hasProperty($destinationProperty->getName())) {
+                $destinationProperty->setValue(
+                    $destinationObject,
+                    $sourceData->getProperty($destinationProperty->getName())->getValue($source),
+                );
+            }
+        }
+
+        return $destinationObject;
+    }
+}
