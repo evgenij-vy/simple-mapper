95b8475dd9f5dc9088914e8ef72e53ec256c8bea
Evgenij Yurchenko <evgenijvy@gmail.com>
2024-08-17 12:23:24 +0300

    add tests

---
+++ tests/BasicTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EvgenijVY\SimpleMapper\UnitTest;
+
+use EvgenijVY\SimpleMapper\Mapper;
+use EvgenijVY\SimpleMapper\UnitTest\Dto\DestinationDto;
+use EvgenijVY\SimpleMapper\UnitTest\Dto\SourceDto;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+
+class BasicTest extends TestCase
+{
+    const TEST_STRING = 'test';
+
+    #[Test]
+    public function basic(): void
+    {
+        $this->assertEquals(
+            self::TEST_STRING,
+            (new Mapper())->map((new SourceDto())->setName(self::TEST_STRING), DestinationDto::class)->getName()
+        );
+    }
+}
